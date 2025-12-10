89be9c1afa832c9944246db78519fa168a9e6d5e
osyol <osyolratik@gmail.com>
2025-12-10 17:25:47 +0500

    Add files via upload

---
+++ billsplitter/src/test/java/uz/billsplitter/billsplitter/BillsplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.billsplitter.billsplitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillsplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
