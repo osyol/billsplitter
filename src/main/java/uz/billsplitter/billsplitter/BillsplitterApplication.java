dcec61b7ab4f378e91bf95b4f8cb231844d38030
osyol <osyolratik@gmail.com>
2025-12-10 16:54:18 +0500

    first commit

---
+++ src/main/java/uz/billsplitter/billsplitter/BillsplitterApplication.java
@@ -0,0 +1,13 @@
+package uz.billsplitter.billsplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillsplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillsplitterApplication.class, args);
+	}
+
+}
